ba43ec7ce00b61254a825d9f5149efe99675b01a
Janjaap Bos <janjaap.bos@capitar.com>
2017-03-01 22:36:11 +0000

    cleanup

--- oi/script.py
+++ oi/script.py
@@ -169,7 +169,6 @@ class Service(service.Service):
             setup_scheduler(self.program)
             self.program.run()
             scheduler.shutdown()
-            #self.logger.info("Stopping")
             logging.info("Stopping")
         self.program.stop_function()
 
@@ -242,12 +241,8 @@ def test_func():
 def test(program):
     job_id = '2b40852613b348b5b595ab07fe875837'
     job = scheduler.get_job(job_id)
-    #program.logger.info('Job 1 ' + str(job))
-    logging.info('Job 1 ' + str(job))
     if not job:
         job = scheduler.add_job(test_func, IntervalTrigger(seconds=10), id=job_id, replace_existing=True)
-        #program.logger.info('Job 2 ' + str(job))
-        logging.info('Job 2 ' + str(job))
     return job
 
 def test(program):
@@ -280,10 +275,8 @@ def setup_scheduler(program):
     program.add_command('get_job', get_job)
     program.add_command('remove_job', remove_job)
     scheduler.start()
-    #program.logger.info('Started scheduler')
     logging.info('Started scheduler')
     test(program)
-    #program.logger.info('Scheduled test job')
     logging.info('Scheduled test job')
 
 
